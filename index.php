e189cefb926af3f88e99a42b25bcd11edf7616e5
Nefthaly <nefthalyabr@gmail.com>
2021-05-31 06:40:21 -0600

    Modificaciones en index.php
    Capturas de datos de la barra de navegación

--- index.php
+++ index.php
@@ -6,17 +6,20 @@ $url =$_GET["url"] ?? "Index/index";
 $arrayUrl = explode("/", $url);
 if(isset($arrayUrl[0])){
     $controller= $arrayUrl[0];
+    echo $controller." ";
 }
 if(isset($arrayUrl[1])){
     if($arrayUrl[1] !=''){
         $method=$arrayUrl[1];
+        echo $method." ";
     }
 }
 
 if(isset($arrayUrl[2])){
     if($arrayUrl[2] !=''){
         $params=$arrayUrl[2];
+        echo $params." ";
     }
 }
-echo $controller." ".$method." ".$params;
+
 ?>
